652d1a719e5fe589e7be8f7f0f69ec7c646d9b11
Pradeep Raj Thapaliya <pradeep.thapaliya95@gmail.com>
2021-10-11 11:56:03 -0500

    data updated

--- download_data.R
+++ download_data.R
@@ -10,14 +10,25 @@ get_date_from_yahoo <- function(tickers, start_date, end_date) {
              ,from = start_date
              ,to = end_date
              ,warnings = FALSE
-             ,auto.assign = TRUE)
+             ,auto.assign = TRUE
+             , src = "yahoo")
   
   
  
+  apple_df <-data.frame(AAPL)
+  apple_df$Date<-rownames(apple_df)
+  rownames(apple_df)<-NULL 
+  write.csv(apple_df,'data/raw_apple_Stock.csv')
   
-  write.csv(AAPL,'data/raw_apple_Stock.csv')
-  write.csv(GOOGL,'data/raw_google_Stock.csv')
-  write.csv(TSLA,'data/raw_tesla_Stock.csv')
+  google_df <-data.frame(GOOGL)
+  google_df$Date<-rownames(google_df)
+  rownames(google_df)<-NULL
+  write.csv(google_df,'data/raw_google_Stock.csv')
+  
+  tesla_df <-data.frame(TSLA)
+  tesla_df$Date<-rownames(tesla_df)
+  rownames(tesla_df)<-NULL
+  write.csv(tesla_df,'data/raw_tesla_Stock.csv')
   
 }
 
@@ -25,5 +36,3 @@ tickers = c("AAPL", "GOOGL", "TSLA")
 start.date <- "2000-01-01"
 today <- format(Sys.Date(), "%Y-%m-%d")
 get_date_from_yahoo(tickers, start.date, today)
-
-
