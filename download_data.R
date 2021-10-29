0bf53ef6a9e56eff1847c02dd13d253c46ea6ffc
Pradeep Raj Thapaliya <pradeep.thapaliya95@gmail.com>
2021-10-29 15:58:12 -0500

    data set and analysis with prophet
    data set
    prophet
    all stock to same file

--- download_data.R
+++ download_data.R
@@ -16,21 +16,38 @@ get_date_from_yahoo <- function(tickers, start_date, end_date) {
   
  
   apple_df <-data.frame(AAPL)
-  apple_df$Date<-rownames(apple_df)
+  apple_df$AAPL.Date<-rownames(apple_df)
+  apple_df$Company.Name <- "APPLE"
   rownames(apple_df)<-NULL 
   write.csv(apple_df,'data/raw_apple_Stock.csv')
   
   google_df <-data.frame(GOOGL)
-  google_df$Date<-rownames(google_df)
+  google_df$GOOGL.Date<-rownames(google_df)
+  google_df$Company.Name <- "GOOGLE"
   rownames(google_df)<-NULL
   write.csv(google_df,'data/raw_google_Stock.csv')
   
   tesla_df <-data.frame(TSLA)
-  tesla_df$Date<-rownames(tesla_df)
+  tesla_df$TSLA.Date<-rownames(tesla_df)
+  tesla_df$Company.Name <- "TESLA"
   rownames(tesla_df)<-NULL
   write.csv(tesla_df,'data/raw_tesla_Stock.csv')
   
+  colnames(tesla_df)
+  
+  colnames(apple_df) <- c("Open","High","Low","Close","Volume","Adjusted","Date", "Company.Name")
+  colnames(google_df) <- c("Open","High","Low","Close","Volume","Adjusted","Date", "Company.Name")
+  colnames(tesla_df) <- c("Open","High","Low","Close","Volume","Adjusted","Date", "Company.Name")
+  
+  #all data
+  all.stock.df <- rbind(apple_df, google_df, tesla_df)
+  write.csv(all.stock.df,'data/all_company_stock.csv')
+  
+  #only date, close and company
+  close.stock.df <- all.stock.df[, c('Date', 'Close', 'Company.Name')]
+  write.csv(close.stock.df,'data/all_company_close_stock.csv')
 }
+ 
 
 tickers = c("AAPL", "GOOGL", "TSLA")
 start.date <- "2000-01-01"
